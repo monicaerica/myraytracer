21af18f677dd8563c95999b27c227f3482a9c401
beca1997 <becattigiacomo@gmail.com>
2022-03-09 12:19:43 +0100

    Added HDRImage class

--- src/main/kotlin/Color.kt
+++ src/main/kotlin/Color.kt
@@ -1,10 +1,10 @@
-public data class Color (var r: Float = 0.0, var g: Float = 0.0, var b: Float = 0.0) {
+public data class Color (var r: Float = 0.0f, var g: Float = 0.0f, var b: Float = 0.0f) {
 
-    operator fun times(this, other: Float): Color{
+    operator fun times(other: Float): Color{
         return Color (this.r + other, this.g + other, this.b + other)
     }
 
-    operator fun plus(this, other: Color): Color{
+    operator fun plus(other: Color): Color{
         return Color(this.r + other.r, this.b + other.b, this.g + other.g)
     }
 }

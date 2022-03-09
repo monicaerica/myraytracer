c6344a7ff407ac76d398f308501a14cd04acc9e1
beca1997 <becattigiacomo@gmail.com>
2022-03-09 11:51:38 +0100

    Added color sum

--- src/main/kotlin/Color.kt
+++ src/main/kotlin/Color.kt
@@ -1,3 +1,6 @@
 public data class Color (var r: Float, var g: Float, var b: Float){
 
+    operator fun plus(this, other: Color): Color{
+        return Color(this.r + other.r, this.b + other.b, this.g + other.g)
+    }
 }

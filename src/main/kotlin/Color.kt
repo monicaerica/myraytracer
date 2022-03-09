bb562fc8ec25097b6aa48c51cc3bcba0fa12ce4c
Monica Erica Sardena <monicaerica.sardena@icloud.com>
2022-03-09 11:48:16 +0100

    Added times overload fun

--- src/main/kotlin/Color.kt
+++ src/main/kotlin/Color.kt
@@ -1,3 +1,7 @@
-public data class Color (var r: Float, var g: Float, var b: Float){
+public data class Color (var r: Float = 0.0, var g: Float = 0.0, var b: Float = 0.0) {
+
+    operator fun times(this, other: Float): Color{
+        return Color (this.r + other, this.g + other, this.b + other)
+    }
 
 }

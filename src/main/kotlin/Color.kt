d8bde67d8e5f3c333b579bbe66cba1cdcf31babe
Monica Erica Sardena <monicaerica.sardena@icloud.com>
2022-03-09 12:22:01 +0100

    added to Color.kt fun is_close()

--- src/main/kotlin/Color.kt
+++ src/main/kotlin/Color.kt
@@ -1,7 +1,14 @@
-public data class Color (var r: Float = 0.0, var g: Float = 0.0, var b: Float = 0.0) {
+public data class Color (var r: Float = 0.0f, var g: Float = 0.0f, var b: Float = 0.0f) {
 
-    operator fun times(this, other: Float): Color{
+    operator fun times(other: Float): Color{
         return Color (this.r + other, this.g + other, this.b + other)
     }
 
+    fun is_close(other: Color): Boolean{
+        var isclose: Boolean = false
+        if ((this.r - other.r) < 1e-5f && (this.g - other.g) < 1e-5f && (this.b - other.b) < 1e-5f) {
+            isclose = true
+        }
+        return isclose
+    }
 }

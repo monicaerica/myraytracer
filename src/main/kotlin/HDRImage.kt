d82567e1195cd90bc184c810736d5c1ad383c06f
beca1997 <becattigiacomo@gmail.com>
2022-03-09 19:08:15 +0100

    Added GetPixel first implementation

--- src/main/kotlin/HDRImage.kt
+++ src/main/kotlin/HDRImage.kt
@@ -3,5 +3,11 @@ data class HDRImage (
     val height: Int,
     var pixels: Array<Color> = Array(width * height) {Color (0.0f,0.0f,0.0f )}
         ) {
+    //get pixel: obtain the 1 dimensional position of a pixel from a tuple (x, y)
+    fun GetPixel(x: Int, y: Int): Color{
+        val pos: Int = x * this.width + y
+        val pixel: Color = this.pixels[pos]
+        return pixel
 
+    }
 }

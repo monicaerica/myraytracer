222ac08109114a410f28d88deca634c3424c51c6
Monica Erica Sardena <monicaerica.sardena@icloud.com>
2022-03-14 16:46:20 +0100

    added valid coordinates()

--- src/main/kotlin/HDRImage.kt
+++ src/main/kotlin/HDRImage.kt
@@ -3,5 +3,11 @@ data class HDRImage (
     val height: Int,
     var pixels: Array<Color> = Array(width * height) {Color (0.0f,0.0f,0.0f )}
         ) {
+
+    fun valid_coordinates(x: Float, y: Float): Boolean{
+        var valid = false
+        if (x < this.width && y < this.height && x > 0 && y > 0) var = true
+        return valid
+    }
             
 }

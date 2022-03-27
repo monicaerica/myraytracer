0d38485b664f6a2ef265cd5be2f33254f93fbdec
beca1997 <becattigiacomo@gmail.com>
2022-03-28 00:00:49 +0200

    Added a class for sRGB

--- src/main/kotlin/Color.kt
+++ src/main/kotlin/Color.kt
@@ -16,3 +16,18 @@ public data class Color (var r: Float = 0.0f, var g: Float = 0.0f, var b: Float 
         return isclose
     }
 }
+
+/**
+ * This class implements a data type for a color with the sRGB encoding
+ * @param: R: Red, value between 0 and 255
+ * @param: G: Green, value between 0 and 255
+ * @param: B: Blue, value between 0 and 255
+ */
+public data class SColor (var R: Int = 0, var G: Int = 0, var B: Int = 0) {
+    /**
+     * Sums two sRGB colors, gives another sRGB color
+     */
+    operator fun plus(other: SColor): SColor {
+        return SColor(this.R + other.R, this.G + other.G, this.B + other.B)
+    }
+}

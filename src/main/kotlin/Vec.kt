806bcf6c8bf230f904b7035efbd095a2066c4284
beca1997 <becattigiacomo@gmail.com>
2022-04-04 17:21:40 +0200

    First implementations for Vec

--- src/main/kotlin/Vec.kt
+++ src/main/kotlin/Vec.kt
@@ -1,1 +1,26 @@
-data class Vec()
+import kotlin.math.sqrt
+
+data class Vec(
+    var x: Float,
+    var y: Float,
+    var z: Float
+) {
+    operator fun plus(other: Vec): Vec{
+      return Vec(x + other.x, y + other.y, z + other.z)
+    }
+    operator fun times(scalar: Float): Vec{
+        return Vec(x * scalar, y * scalar, z * scalar)
+    }
+
+    operator fun times(other: Vec): Float{
+        return x * other.x + y * other.y + z * other.z
+    }
+
+    fun SquaredNorm (): Float{
+        return this * this
+    }
+
+    fun Norm(): Float{
+        return sqrt(SquaredNorm())
+    }
+}

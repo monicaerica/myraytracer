0b756b2053fdcc3ad35c526400798d190890a5e5
beca1997 <becattigiacomo@gmail.com>
2022-04-11 15:09:32 +0200

    Added toNormal

--- src/main/kotlin/Vec.kt
+++ src/main/kotlin/Vec.kt
@@ -5,22 +5,60 @@ data class Vec(
     var y: Float,
     var z: Float
 ) {
-    operator fun plus(other: Vec): Vec{
-      return Vec(x + other.x, y + other.y, z + other.z)
+
+    /**
+     * Returns the sum of two vectors
+     * @param other: The other vector to be summed with the original
+     * @return A Vec given by the sum of the original and the other vector
+     */
+    operator fun plus(other: Vec): Vec {
+        return Vec(x + other.x, y + other.y, z + other.z)
     }
-    operator fun times(scalar: Float): Vec{
+
+    /**
+     * Performs the product between a vector and a scalar
+     * @param scalar: A float to be multiplied with the vector
+     * @return The vector times the scalar
+     */
+    operator fun times(scalar: Float): Vec {
         return Vec(x * scalar, y * scalar, z * scalar)
     }
 
-    operator fun times(other: Vec): Float{
+    /**
+     * Negates a vector
+     */
+    fun Neg(): Vec{
+        return this * -1.0f
+    }
+
+    operator fun minus(other: Vec): Vec{
+        return this + other.Neg()
+    }
+
+    operator fun times(other: Vec): Float {
         return x * other.x + y * other.y + z * other.z
     }
 
-    fun SquaredNorm (): Float{
+    fun SquaredNorm(): Float {
         return this * this
     }
 
-    fun Norm(): Float{
+    fun Norm(): Float {
         return sqrt(SquaredNorm())
+    }
+
+    fun Cross(other: Vec): Vec {
+        return Vec(
+            y * other.z - z * other.y,
+            z * other.x - x * other.z,
+            x * other.y - y * other.x
+        )
+    }
+
+    fun Normalize(): Vec {
+        return Vec(x / Norm(), y / Norm(), z / Norm())
+    }
+    fun toNormal(): Normal{
+        return Normal(x, y, z)
     }
 }
---
+++ src/main/kotlin/VecTest.kt
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.Assertions.*
+
+internal class VecTest {
+
+    @org.junit.jupiter.api.Test
+    fun plus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun times() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun neg() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun minus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun testTimes() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun squaredNorm() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun norm() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun cross() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun normalize() {
+    }
+}

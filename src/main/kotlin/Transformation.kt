0f308d29b6f906a219057fd41c476d7e8d035d21
beca1997 <becattigiacomo@gmail.com>
2022-04-19 11:51:17 +0200

    Added scaling

--- src/main/kotlin/Transformation.kt
+++ src/main/kotlin/Transformation.kt
@@ -3,5 +3,16 @@ class Transformation(
     var InvM: HomMatrix = HomMatrix()
 )
 {
-        
+
+    fun Scaling(vec: Vec): Transformation{
+        val M: HomMatrix = HomMatrix(floatArrayOf(vec.x, 0.0f, 0.0f, 0.0f,
+            0.0f, vec.y, 0.0f, 0.0f,
+            0.0f, 0.0f, vec.z, 0.0f,
+            0.0f, 0.0f, 0.0f, 1.0f))
+        val InvM: HomMatrix = HomMatrix(floatArrayOf(1.0f / vec.x, 0.0f, 0.0f, 0.0f,
+            0.0f, 1.0f / vec.y, 0.0f, 0.0f,
+            0.0f, 0.0f, 1.0f / vec.z, 0.0f,
+            0.0f, 0.0f, 0.0f, 1.0f))
+        return Transformation(M, InvM)
+    }
 }

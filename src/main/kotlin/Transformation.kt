b8d4ade912d2348d1f014a84236a42fd963e021f
beca1997 <becattigiacomo@gmail.com>
2022-04-19 12:18:35 +0200

    Added Transformation * Vector

--- src/main/kotlin/Transformation.kt
+++ src/main/kotlin/Transformation.kt
@@ -8,19 +8,41 @@ class Transformation(
 )
 {
 
+    operator fun times(other: Vec): Vec{
+        val row0 = M.elements.slice(0..3)
+        val row1 = M.elements.slice(4..7)
+        val row2 = M.elements.slice(8..11)
+        val row3 = M.elements.slice(12..15)
+
+        return Vec(
+            row0[0] * other.x + row0[1] * other.y + row0[2] * other.z,
+            row1[0] * other.x + row1[1] * other.y + row1[2] * other.z,
+            row2[0] * other.x + row2[1] * other.y + row2[2] * other.z,
+        )
+    }
+
+
+
+
+    /**
+     * Exchange inverse matrix and matrix
+     */
     fun Inverse(): Transformation{
         return Transformation(InvM, M)
     }
 
+    /**
+     *
+     */
     fun Scaling(vec: Vec): Transformation{
         val M: HomMatrix = HomMatrix(floatArrayOf(vec.x, 0.0f, 0.0f, 0.0f,
-            0.0f, vec.y, 0.0f, 0.0f,
-            0.0f, 0.0f, vec.z, 0.0f,
-            0.0f, 0.0f, 0.0f, 1.0f))
+                                                    0.0f, vec.y, 0.0f, 0.0f,
+                                                    0.0f, 0.0f, vec.z, 0.0f,
+                                                    0.0f, 0.0f, 0.0f, 1.0f))
         val InvM: HomMatrix = HomMatrix(floatArrayOf(1.0f / vec.x, 0.0f, 0.0f, 0.0f,
-            0.0f, 1.0f / vec.y, 0.0f, 0.0f,
-            0.0f, 0.0f, 1.0f / vec.z, 0.0f,
-            0.0f, 0.0f, 0.0f, 1.0f))
+                                                    0.0f, 1.0f / vec.y, 0.0f, 0.0f,
+                                                    0.0f, 0.0f, 1.0f / vec.z, 0.0f,
+                                                    0.0f, 0.0f, 0.0f, 1.0f))
         return Transformation(M, InvM)
     }
 

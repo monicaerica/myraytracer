18ed797c48d2b2c4ebf5b7006a69ad0b621f055a
beca1997 <becattigiacomo@gmail.com>
2022-04-19 12:06:39 +0200

    Added Rotations Along xyz axis

--- src/main/kotlin/Transformation.kt
+++ src/main/kotlin/Transformation.kt
@@ -1,3 +1,7 @@
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+
 class Transformation(
     var M: HomMatrix = HomMatrix(),
     var InvM: HomMatrix = HomMatrix()
@@ -31,6 +35,66 @@ class Transformation(
                                                     0.0f, 0.0f, 0.0f, 1.0f))
         return Transformation(M, InvM)
     }
+
+    /**
+     * Rotate by an angle around the x axis
+     * @param  ang: The value of the angle in degrees
+     */
+    fun RotationX(ang: Float): Transformation{
+        var RadAng = ang * (PI.toFloat() / 180.0f)
+        val COSang = cos(RadAng)
+        val SINang = sin(RadAng)
+
+        val M: HomMatrix = HomMatrix(floatArrayOf(1.0f, 0.0f, 0.0f, 0.0f,
+                                                0.0f, COSang, -SINang, 0.0f,
+                                                0.0f, SINang, COSang, 0.0f,
+                                                0.0f, 0.0f, 0.0f, 1.0f))
+        val InvM: HomMatrix = HomMatrix(floatArrayOf(1.0f, 0.0f, 0.0f, 0.0f,
+                                                0.0f, COSang, SINang, 0.0f,
+                                                0.0f, -SINang, COSang, 0.0f,
+                                                0.0f, 0.0f, 0.0f, 1.0f))
+        return Transformation(M, InvM)
+    }
+
+    /**
+     * Rotate by an angle around the y axis
+     * @param  ang: The value of the angle in degrees
+     */
+    fun RotationY(ang: Float): Transformation{
+        var RadAng = ang * (PI.toFloat() / 180.0f)
+        val COSang = cos(RadAng)
+        val SINang = sin(RadAng)
+
+        val M: HomMatrix = HomMatrix(floatArrayOf(COSang, 0.0f, SINang, 0.0f,
+                                                0.0f, 1.0f, 0.0f, 0.0f,
+                                                -SINang, 0.0f, COSang, 0.0f,
+                                                0.0f, 0.0f, 0.0f, 1.0f))
+        val InvM: HomMatrix = HomMatrix(floatArrayOf((COSang, 0.0f, -SINang, 0.0f,
+                                                0.0f, 1.0f, 0.0f, 0.0f,
+                                                SINang, 0.0f, COSang, 0.0f,
+                                                0.0f, 0.0f, 0.0f, 1.0f))
+        return Transformation(M, InvM)
+    }
+    /**
+     * Rotate by an angle around the z axis
+     * @param  ang: The value of the angle in degrees
+     */
+    fun RotationZ(ang: Float): Transformation{
+        var RadAng = ang * (PI.toFloat() / 180.0f)
+        val COSang = cos(RadAng)
+        val SINang = sin(RadAng)
+
+        val M: HomMatrix = HomMatrix(floatArrayOf(COSang, -SINang, 0.0f, 0.0f,
+                                                    SINang, COSang, 0.0f, 0.0f,
+                                                    0.0f, 0.0f, 1.0f, 0.0f,
+                                                    0.0f, 0.0f, 0.0f, 1.0f))
+        val InvM: HomMatrix = HomMatrix(floatArrayOf(COSang, SINang, 0.0f, 0.0f,
+                                                    -SINang, COSang, 0.0f, 0.0f,
+                                                    0.0f, 0.0f, 1.0f, 0.0f,
+                                                    0.0f, 0.0f, 0.0f, 1.0f)))
+        return Transformation(M, InvM)
+    }
+
 
 
 }

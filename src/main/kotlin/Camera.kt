ec1878d13106e89dffe6287c9be563785aaf953a
beca1997 <becattigiacomo@gmail.com>
2022-04-25 12:08:23 +0200

    Added docstring

--- src/main/kotlin/Camera.kt
+++ src/main/kotlin/Camera.kt
@@ -7,7 +7,7 @@ interface Camera {
     fun FireRay(u: Float, v: Float): Ray
 }
 
-/*
+
 class OrthogonalCamera (override val AspectRatio: Float = 1.0f, override val trans: Transformation,
                         override val distance: Float = 0.0f): Camera{
 
@@ -19,12 +19,12 @@ class OrthogonalCamera (override val AspectRatio: Float = 1.0f, override val tra
 }
 
 class PerpectiveCamera ( override val AspectRatio: Float = 1.0f, override val trans: Transformation,
-                         override val distance: Float = 1.0f): Camera{
+                         override val distance: Float = 1.0f): Camera {
 
     override fun FireRay(u: Float, v: Float): Ray {
         val origin: Point = Point(-this.distance, 0.0f, 0.0f)
         val direction: Vec = Vec(this.distance, (1.0f - 2.0f * u) * AspectRatio, 2.0f * v - 1.0f)
         return Ray(origin, direction, tmin = 1.0f).transform(this.trans)
     }
+}
 
-}*/
--- src/main/kotlin/Ray.kt
+++ src/main/kotlin/Ray.kt
@@ -1,5 +1,13 @@
 import kotlin.Float.Companion.POSITIVE_INFINITY
 
+/**
+ * This class implements the ray data type used in the ray tracing algorithm
+ * @param Origin: A point in space (x, y, z), is the origin of the ray
+ * @param Dir: A vector, gives the direction of the ray
+ * @param tmin: A float which gives the minimum "time"
+ * @param tmax: A float which gives the maximum "time"
+ * @param Depth:
+ */
 data class Ray(
     val Origin: Point = Point(),
     val Dir: Vec = Vec(),
@@ -8,15 +16,30 @@ data class Ray(
     val Depth: Int = 0
 
 ) {
-
-    fun IsClose(other: Ray, epsilon: Float = 1e-5f): Boolean{
+    /**
+     * Checks whether two rays have the same values for origin and direction whithin a given epsilon to account for float approximation
+     * @param other: The other ray used in the comparison
+     * @param epsilon: The parameter for the approximation
+     * @return IsClose: True if they are close
+     */
+    fun IsClose(other: Ray, epsilon: Float = 1e-5f): Boolean {
         return Origin.is_close(other.Origin, epsilon = epsilon) and Dir.IsClose(other.Dir, epsilon = epsilon)
     }
 
-    fun At(t: Float): Point{
+    /**
+     * Gives the point in 3d space on the ray after a given "time" t
+     * @param t: The "time" at which you want to know the location along the ray
+     * @return a point in space
+     */
+    fun At(t: Float): Point {
         return Origin + Dir * t
     }
 
+    /**
+     * Trasform a ray using the possible transformations: scaling, rotate around x, y or z axis, translate
+     * @param trans: the transformation you want to apply to the ray
+     * @return the transformed ray
+     */
     fun transform(trans: Transformation): Ray {
         return Ray(
             trans * this.Origin,
@@ -24,8 +47,6 @@ data class Ray(
             this.tmin,
             this.tmax,
             this.Depth
-        ) 
+        )
     }
-
-
 }

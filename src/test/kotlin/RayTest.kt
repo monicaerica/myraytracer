edee999a52b1c630fae1138da458f1a61968e4d4
beca1997 <becattigiacomo@gmail.com>
2022-04-26 21:49:01 +0200

    Raytest

--- src/test/kotlin/RayTest.kt
+++ src/test/kotlin/RayTest.kt
@@ -1,3 +1,14 @@
-import org.junit.jupiter.api.Assertions.*
+import org.junit.Assert.*
+import org.junit.Test
+internal class RayTest{
+    @Test
+    fun IsClose(){
+        val ray1: Ray = Ray(Origin = Point(1.0f, 2.0f, 3.0f), Dir = Vec(5.0f, 4.0f, -1.0f))
+        val ray2: Ray = Ray(Origin = Point(1.0f, 2.0f, 3.0f), Dir = Vec(5.0f, 4.0f, -1.0f))
+        val ray3: Ray = Ray(Origin = Point(5.0f, 1.0f, 4.0f), Dir = Vec(3.0f, 9.0f, 4.0f))
 
-internal class RayTest
+        assertTrue(ray1.IsClose(ray2))
+        assertFalse(ray1.IsClose(ray3))
+
+    }
+}

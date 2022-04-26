5096359374f860e277265a571f4002848a1c2334
beca1997 <becattigiacomo@gmail.com>
2022-04-26 21:48:49 +0200

    ImagetracerTest

--- src/test/kotlin/ImageTracerTest.kt
+++ src/test/kotlin/ImageTracerTest.kt
@@ -1,3 +1,12 @@
-import org.junit.jupiter.api.Assertions.*
+import org.junit.Assert.*
+import org.junit.Test
 
-internal class ImageTracerTest
+internal class ImageTracerTest {
+/*    @Test
+    fun FireAllRays(){
+        var image = HDRImage(width = 4, height = 2)
+        var camera = PerpectiveCamera(AspectRatio = 2.0f)
+        var tracer = ImageTracer(image = image, camera = camera)
+
+    }*/
+}

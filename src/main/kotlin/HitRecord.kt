08ac2e23dd4c2c97abee3c2dfa9d4119306b8054
beca1997 <becattigiacomo@gmail.com>
2022-04-27 12:09:13 +0200

    created Hitrecord, Vec2d

--- src/main/kotlin/HitRecord.kt
+++ src/main/kotlin/HitRecord.kt
@@ -1,2 +1,8 @@
-class HitRecord {
+data class HitRecord(
+    var worldPoint: Point = Point(),
+    var normal: Normal = Normal(),
+    var surfacePoint: Vec2d = Vec2d(),
+    var t: Float = 0.0f,
+    var ray: Ray = Ray()
+) {
 }

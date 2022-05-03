593eb5c298d77c035dfd80194b3d7603f63c604d
Monica Erica Sardena <monicaerica.sardena@icloud.com>
2022-05-03 21:30:44 +0200

    added partial WorldTest

---
+++ src/main/kotlin/WolrdTest.kt
@@ -0,0 +1,33 @@
+import org.junit.Assert
+import org.junit.Test
+import org.junit.Assert.*
+
+internal class WolrdTest {
+    companion object {
+        val VEC_X : Vec = Vec(1.0f, 0.0f, 0.0f)
+        val VEC_Y : Vec = Vec(0.0f, 1.0f, 0.0f)
+        val VEC_Z : Vec = Vec(0.0f, 0.0f, 1.0f)
+        var sphere1: Sphere = Sphere(Translation(VEC_X * 2.0f))
+        var sphere2: Sphere = Sphere(Translation(VEC_X * 8.0f))
+    }
+
+    @Test
+    fun TestRayIntersections(){
+        var world = World()
+        var intersection1 : HitRecord?
+        var intersection2 : HitRecord?
+        world.AddShape(sphere1)
+        world.AddShape(sphere2)
+
+        intersection1 = world.rayIntersection(Ray(Point(0.0f, 0.0f, 0.0f), VEC_X))
+//        if (intersection1 != null) {
+//            assert(intersection1.worldPoint.is_close(Point(1.0f, 0.0f, 0.0f)))
+//        }
+        assertNotNull(intersection1)
+        intersection2 = world.rayIntersection(Ray(Point(10.0f, 0.0f, 0.0f), VEC_X * -1.0f))
+//        if (intersection2 != null) {
+//            assert(intersection2.worldPoint.is_close(Point(9.0f, 0.0f, 0.0f)))
+//        }
+        assertNotNull(intersection2)
+    }
+}

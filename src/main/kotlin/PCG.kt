601365f15683d445d424ffc71f4f9ff5fbce5efa
Monica Erica Sardena <monicaerica.sardena@icloud.com>
2022-05-13 15:53:15 +0200

    Implemented PCGclass + PCGTests

--- src/main/kotlin/PCG.kt
+++ src/main/kotlin/PCG.kt
@@ -1,2 +1,29 @@
-class PCG {
+//A Data Class is like a regular class but with some additional functionalities.
+//
+//With Kotlin’s data classes, you don’t need to write/generate all the lengthy
+//boilerplate code yourself. The compiler automatically generates a default
+//getter and setter for all the mutable properties, and a getter (only)
+//for all the read-only properties of the data class. Moreover, It also
+//derives the implementation of standard methods like equals(), hashCode()
+//and toString() from the properties declared in the data class’s primary
+//constructor.
+//https://www.callicoder.com/kotlin-data-classes/
+
+class PCG(val init_state : ULong = 42u, val init_seq : ULong = 54u,
+               var state : ULong = 0u,
+               var inc : ULong = 0u){
+    init{
+        this.inc = (init_seq shl 1) or 1u
+        this.Random()
+        this.state += init_state
+        this.Random()
+    }
+
+    fun Random(): UInt{
+        var oldstate = this.state
+        this.state = oldstate * 6364136223846793005u + this.inc
+        var xorshifted = (((oldstate shr 18) xor oldstate) shr 27).toUInt()
+        var rot = (oldstate shr 59).toUInt()
+        return ((xorshifted shr rot.toInt()) or ( xorshifted shl ((-rot.toInt()) and 31).toInt())).toUInt()
+    }
 }

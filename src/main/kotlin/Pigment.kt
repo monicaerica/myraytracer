babee29c3aa0292e851ad3fcf96f3607c3acd874
Monica Erica Sardena <monicaerica.sardena@icloud.com>
2022-05-14 12:17:05 +0200

    Added CheckredPigment() in Pigment()

--- src/main/kotlin/Pigment.kt
+++ src/main/kotlin/Pigment.kt
@@ -1,3 +1,5 @@
+import kotlin.math.roundToInt
+
 /**
   *  This represents a class that maps every point of a surface(2DVec) in a Color
 */
@@ -12,8 +14,7 @@ abstract class Pigment() {
 /**
  *   This is the case of an uniform surface
 */
-class UniformPigment(color: Color) : Pigment(){
-    val color = color
+class UniformPigment(val color: Color) : Pigment(){
     /**
      *  In this case the function returns the same color whichever Vec2d
      *  is given
@@ -21,5 +22,25 @@ class UniformPigment(color: Color) : Pigment(){
      */
     override fun GetColor(uv: Vec2d): Color{
         return this.color
+    }
+}
+
+/**
+ *   This is useful for debugging purposes
+ *   @param col1 one of the two colors of the chessboard
+ *   @param col2 the second color
+ *   @param n_steps number of squares for each side of the chessboard
+ */
+class CheckredPigment(val col1 : Color, val col2 : Color, val n_steps : Int = 10) : Pigment(){
+    /**
+     *  This returns the color of the chessboard at the point given
+     *  @param uv the Vec2d pointing to the area you want to know the color
+     *  @return : color, variable of type Color
+     */
+    override fun GetColor(uv: Vec2d): Color{
+        var int_u = (uv.u * this.n_steps).roundToInt()
+        var int_v = (uv.v * this.n_steps).roundToInt()
+        return if ((int_u % 2) == (int_v % 2)) this.col1
+        else this.col2
     }
 }
